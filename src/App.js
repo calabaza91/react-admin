afa07dc2815ced43aa0c65da7884f097017893ef
Caleb Cox <calebcox2010@gmail.com>
2024-07-02 01:45:16 -0500

    Add Invoices scene

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidenav from "./scenes/global/Sidenav";
-import Dashboard from "./scenes/dashboard/Dashboard";
+import Dashboard from "./scenes/dashboard/dashboard";
 import Team from "./scenes/team/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices/invoices";
 import Contacts from "./scenes/contacts/contacts";
 // import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
@@ -33,7 +33,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
