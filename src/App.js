60178316e9f3cb952f3745ee9662ee49ee34c4ec
Caleb Cox <calebcox2010@gmail.com>
2024-07-02 01:29:29 -0500

    Add Contacts scene

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidenav from "./scenes/global/Sidenav";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/team";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts/contacts";
 // import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
@@ -32,7 +32,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
