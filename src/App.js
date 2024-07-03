0eaace8c7a2445a85f5f69df8485c48b381df33c
Caleb Cox <calebcox2010@gmail.com>
2024-07-02 19:59:15 -0500

    Add Form scene

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./scenes/dashboard/dashboard";
 import Team from "./scenes/team/team";
 import Invoices from "./scenes/invoices/invoices";
 import Contacts from "./scenes/contacts/contacts";
-// import Form from "./scenes/form";
+import Form from "./scenes/form/form";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
@@ -34,7 +34,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
